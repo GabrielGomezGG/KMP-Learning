930d6a087f28a8c47098e31ada96809defcaf1ec
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-12-04 22:04:41 -0300

    feat: example navegation with Voyager

--- VoyagerExample/composeApp/src/commonMain/kotlin/App.kt
+++ VoyagerExample/composeApp/src/commonMain/kotlin/App.kt
@@ -1,38 +1,88 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.transitions.FadeTransition
+import cafe.adriel.voyager.transitions.ScaleTransition
+import cafe.adriel.voyager.transitions.SlideTransition
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     MaterialTheme {
-        var greetingText by remember { mutableStateOf("Hello World!") }
-        var showImage by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = {
-                greetingText = "Compose: ${Greeting().greet()}"
-                showImage = !showImage
-            }) {
-                Text(greetingText)
+        Navigator(Wea1()){
+            SlideTransition(it)
+            //ScaleTransition(it)
+            //FadeTransition(it)
+        }
+    }
+}
+
+class Wea1 : Screen{
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.current
+        Box(
+            modifier = Modifier.fillMaxSize().background(Color.Cyan),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(onClick = { navigator?.push(Wea2()) }) {
+                Text("Go to Wea2")
             }
-            AnimatedVisibility(showImage) {
-                Image(
-                    painterResource("compose-multiplatform.xml"),
-                    null
-                )
+        }
+    }
+}
+
+class Wea2 : Screen{
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.current
+        Box(
+            modifier = Modifier.fillMaxSize().background(Color.Gray),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.SpaceAround,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(onClick = {
+                    navigator?.push(Wea3())
+                }) {
+                    Text("Go to Wea3")
+                }
+                Button(onClick = {
+                    navigator?.pop()
+                }) {
+                    Text("Go to Wea1")
+                }
+            }
+        }
+    }
+}
+
+class Wea3 : Screen{
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.current
+        Box(
+            modifier = Modifier.fillMaxSize().background(Color.Red),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(onClick = { navigator?.push(Wea1()) }) {
+                Text("Go to Wea1")
             }
         }
     }
